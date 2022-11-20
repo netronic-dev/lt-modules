69ecf33e5b8f264cb1d3b2b12f8daee7ae4c2aad
Vladislav RedSide <vls.sidorow@gmail.com>
2022-11-21 00:55:25 +0200

    update black friday modules

---
+++ BlackFriday/Cards/index.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import style from "./style.module.scss";
+
+const Cards = (props) => {
+	return (
+		<section className={style.section} name={props.name}>
+			<div className={style.wrapper}>
+				<h2 className={style.title}>{props.title}</h2>
+				<div className={style.cards_wrapper}>
+					{props.data?.map((item, index) => (
+						<CardCell
+							key={index}
+							image={item.image}
+							title={item.title}
+							text={item.text}
+							buttonText={item.buttonText}
+							price={item.price}
+							discount={item.discount}
+							color={item.color}
+							onClick={() => props.onClick(item)}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Cards;
+
+const CardCell = (props) => {
+	return (
+		<div className={style.cell}>
+			<div className={style.image_outer}>
+				<div className={style.discount} style={{ background: props.color }}>
+					<div className={style.discount__title}>{props.discount.title}</div>
+					<div className={style.discount__text}>{props.discount.text}</div>
+				</div>
+				{props.image.src && (
+					<Image
+						src={props.image.src}
+						layout="fill"
+						objectFit="cover"
+						objectPosition={props.image.objectPosition || "0 0"}
+					/>
+				)}
+			</div>
+			<div className={style.cell_text_outer}>
+				<div className={style.cell_top}>
+					<h3 className={style.cell__title}>{props.title}</h3>
+					<p className={style.cell__text}>{props.text}</p>
+				</div>
+				<div className={style.cell_bottom}>
+					<div className={style.cell_price}>
+						<div
+							className={style.cell_price__old}
+							style={{ color: props.color }}
+						>
+							{props.price.old}
+						</div>
+						<div
+							className={style.cell_price__new}
+							style={{ color: props.color }}
+						>
+							{props.price.new}
+						</div>
+					</div>
+					<button
+						style={{ background: props.color }}
+						className={style.cell__button}
+						onClick={props.onClick}
+					>
+						{props.buttonText}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
--- BlackFriday/ThankYou/index.js
+++ BlackFriday/ThankYou/index.js
@@ -1,19 +1,23 @@
-import Link from "next/link"
-import style from "./style.module.scss"
+import Image from "next/image";
+import style from "./style.module.scss";
 
 export default function IAAPAThankYou(props) {
-  return (
-    <section className={style.thank_you}>
-      <div className={style.main}>
-        <div className={style.main_in}>
-          <h1 className={style.title}>
-            {props.title}
-          </h1>
-          <p className={style.subtitle}>
-            {props.subtitle}
-          </p>
-        </div>
-      </div>
-    </section>
-  )
+	return (
+		<section className={style.section}>
+			<div className={style.main}>
+				<div className={style.main_in}>
+					<h1 className={style.title}>{props.title}</h1>
+					<p className={style.subtitle}>{props.subtitle}</p>
+				</div>
+				<div className={style.image}>
+					<Image
+						src={props.image}
+						layout="responsive"
+						width={1170}
+						height={500}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }
