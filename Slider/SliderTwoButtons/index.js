8f05cbe4f6413e4da11e47de90b0b6188effe8aa
Artem <aliens199@ukr.net>
2023-01-21 12:41:16 +0200

    Add page galaxy eclipse

--- Slider/SliderTwoButtons/index.js
+++ Slider/SliderTwoButtons/index.js
@@ -1,45 +1,51 @@
-import Slider from "react-slick";
-import React, { Component } from "react";
-import Image from "next/image"
+import Slider from 'react-slick';
+import React, { Component } from 'react';
+import Image from 'next/image';
+
 export default class SliderTwoButtons extends Component {
-  render() {
-    let buttonTitles = this.props.listofTitles;
-    const settings = {
-      customPaging: function (i) {
+    render() {
+        let buttonTitles = this.props.listofTitles;
+        const settings = {
+            customPaging: function (i) {
+                return (
+                    <div>
+                        <button>{buttonTitles[i++]}</button>
+                    </div>
+                );
+            },
+            dots: true,
+            infinite: true,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        };
         return (
-          <div>
-            <button>{buttonTitles[i++]}</button>
-          </div>
+            <div className='sliderTwoButtons'>
+                <Slider {...settings}>
+                    {this.props.sliderData.map((data, index) => (
+                        <ImageAdder
+                            src={data.image}
+                            key={index}
+                            height={data.imgHeight}
+                        />
+                    ))}
+                </Slider>
+            </div>
         );
-      },
-      dots: true,
-      infinite: true,
-      arrows: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-    return (
-      <div className="sliderTwoButtons">
-        <Slider {...settings}>
-          {this.props.sliderData.map((data, index) => (
-            <ImageAdder
-              src={data.image}
-              text={data.text}
-              key={index}
-            />
-          ))}
-        </Slider>
-      </div>
-    );
-  }
+    }
 }
 
 function ImageAdder(props) {
-  return (
-    <div>
-      <p>{props.text}</p>
-      <Image src={props.src} layout="responsive" width={1180} height={485} />
-    </div>
-  );
+    return (
+        <div>
+            <p>{props.text}</p>
+            <Image
+                src={props.src}
+                layout='responsive'
+                width={1180}
+                height={props.height || '485'}
+            />
+        </div>
+    );
 }
