a382020cb445bcc8b7331c4e79e4d05337a3b6ee
artemyatsenko91 <aliens199@ukr.net>
2023-02-15 22:59:10 +0200

    Fix map country sales

--- Map/MapSales.js
+++ Map/MapSales.js
@@ -1,75 +1,64 @@
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { scaleQuantize } from "d3-scale";
-import { csv } from "d3-fetch";
-import { useEffect, useState } from "react";
-import style from "./style.module.scss"
-import { useInView } from "react-hook-inview";
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import { scaleQuantize } from 'd3-scale';
+import { csv } from 'd3-fetch';
+import { useEffect, useState } from 'react';
+import style from './style.module.scss';
+import { useInView } from 'react-hook-inview';
 
-const geoUrl = "/map.json";
+const geoUrl = '/map.json';
 
-const colorScale = scaleQuantize()
-  .domain([1, 4])
-  .range([
-    "#0090FF",
-    "#0090FF",
-    "#0090FF",
-    "#ffffff"
-  ]);
+const colorScale = scaleQuantize().domain([1, 2]).range(['#dddddd', '#0090FF']);
 
 export function MapSales(props) {
+    const [data, setData] = useState([]);
 
-  const [data, setData] = useState([]);
+    useEffect(() => {
+        csv(`/countries-sales.csv`).then((csvData) => {
+            setData(csvData);
+        });
+    }, []);
 
-  useEffect(() => {
+    const [ref, isVisible] = useInView();
 
-    csv(`/countries-sales.csv`).then((csvData) => {
-      setData(csvData);
-    });
-
-  }, []);
-
-  const [ref, isVisible] = useInView()
-
-  return (
-    <div className={style.map} ref={ref}>
-      <ComposableMap
-      >
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => {
-              const cur = data.find(item => item.id === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  style={{
-                    default: {
-                      outline: "none",
-                      stroke: "#EDEEF0",
-                      strokeWidth: "1",
-                    },
-                    active: {
-                      fill: "#ffffff",
-                      outline: "none"
-                    },
-                    hover: {
-                      fill: "#0091ffcb",
-                      outline: "none",
-                      stroke: "#EDEEF0",
-                      strokeWidth: "2"
-                    },
-                    pressed: {
-                      fill: "#0090FF",
-                      outline: "none"
-                    },
-                  }}
-                  fill={colorScale(cur ? cur.rate : "#EEE")}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ComposableMap >
-    </div>
-  )
+    return (
+        <div className={style.map} ref={ref} data-tip=''>
+            <ComposableMap>
+                <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                        geographies.map((geo) => {
+                            const cur = data.find((item) => item.id === geo.id);
+                            return (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    style={{
+                                        default: {
+                                            outline: 'none',
+                                            stroke: '#EDEEF0',
+                                            strokeWidth: '1',
+                                        },
+                                        active: {
+                                            fill: '#ffffff',
+                                            outline: 'none',
+                                        },
+                                        hover: {
+                                            fill: '#0091ffcb',
+                                            outline: 'none',
+                                            stroke: '#EDEEF0',
+                                            strokeWidth: '2',
+                                        },
+                                        pressed: {
+                                            fill: '#0090FF',
+                                            outline: 'none',
+                                        },
+                                    }}
+                                    fill={colorScale(cur ? cur.rate : '#EEE')}
+                                />
+                            );
+                        })
+                    }
+                </Geographies>
+            </ComposableMap>
+        </div>
+    );
 }
