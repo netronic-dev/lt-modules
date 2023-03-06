0db10e3036b4383a1aabb4b88e875707ba2a2607
artemyatsenko91 <aliens199@ukr.net>
2023-03-06 22:29:05 +0200

    Fix laser-tag-convention-2023/thank-you

--- Event/ThankYou/Main/index.tsx
+++ Event/ThankYou/Main/index.tsx
@@ -1,20 +1,25 @@
-import { FunctionComponent, ReactNode } from "react";
-import style from "./style.module.scss";
+import { FunctionComponent, ReactNode } from 'react';
+import style from './style.module.scss';
 
 interface ThankYouProps {
-	title: ReactNode;
-	subtitle: string;
+    title: ReactNode;
+    subtitle: string;
+    lt_convention?: boolean;
 }
 
 const ThankYou: FunctionComponent<ThankYouProps> = (props) => {
-	return (
-		<section className={style.thank_you}>
-			<div className={style.main}>
-				<h1 className={style.title}>{props.title}</h1>
-				<p className={style.subtitle}>{props.subtitle}</p>
-			</div>
-		</section>
-	);
+    return (
+        <section
+            className={
+                props.lt_convention ? style.thank_you_conv : style.thank_you
+            }
+        >
+            <div className={style.main}>
+                <h1 className={style.title}>{props.title}</h1>
+                <p className={style.subtitle}>{props.subtitle}</p>
+            </div>
+        </section>
+    );
 };
 
 export default ThankYou;
