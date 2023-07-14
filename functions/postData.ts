ea1c86469702175a91af18590fdbe7e77996bffc
artemyatsenko91 <aliens199@ukr.net>
2023-07-14 07:42:32 +0300

    Add headers

--- functions/postData.ts
+++ functions/postData.ts
@@ -1,67 +1,72 @@
 import axios from 'axios';
 
 export async function postData(
-    values: any,
-    url: string,
-    orderName: string,
-    lang: string,
-    siteDomain: string,
-    routerQuerry?: any,
-    fields?: field[] | []
+	values: any,
+	url: string,
+	orderName: string,
+	lang: string,
+	siteDomain: string,
+	routerQuerry?: any,
+	fields?: field[] | [],
 ) {
-    let locationInfo: any = await getLocationData();
-    let data: any = {
-        siteName: siteDomain,
-        orderName: orderName,
-        name: values.name || '',
-        email: values.email || '',
-        phone: values.phone || '',
-        lang: lang,
-        query: routerQuerry
-            ? {
-                  utm_campaign: routerQuerry.utm_campaign || '',
-                  utm_medium: routerQuerry.utm_medium || '',
-                  utm_source: routerQuerry.utm_source || '',
-                  utm_term: routerQuerry.utm_term || '',
-              }
-            : {},
-        fields: [
-            {
-                name: 'Страна',
-                value: locationInfo.country,
-                BXName: 'ADDRESS_COUNTRY',
-            },
-            {
-                name: 'Город',
-                value: locationInfo.city,
-                BXName: 'ADDRESS_CITY',
-            },
-            {
-                name: 'Регион',
-                value: locationInfo.region,
-                BXName: 'UF_CRM_1517920515',
-            },
-            {
-                name: 'IP',
-                value: locationInfo.ip,
-            },
-        ],
-    };
-    if (fields) {
-        data.fields = [...data.fields, ...fields];
-    }
-    return await axios.post(url, data);
+	let locationInfo: any = await getLocationData();
+	let data: any = {
+		siteName: siteDomain,
+		orderName: orderName,
+		name: values.name || '',
+		email: values.email || '',
+		phone: values.phone || '',
+		lang: lang,
+		query: routerQuerry
+			? {
+					utm_campaign: routerQuerry.utm_campaign || '',
+					utm_medium: routerQuerry.utm_medium || '',
+					utm_source: routerQuerry.utm_source || '',
+					utm_term: routerQuerry.utm_term || '',
+			  }
+			: {},
+		fields: [
+			{
+				name: 'Страна',
+				value: locationInfo.country,
+				BXName: 'ADDRESS_COUNTRY',
+			},
+			{
+				name: 'Город',
+				value: locationInfo.city,
+				BXName: 'ADDRESS_CITY',
+			},
+			{
+				name: 'Регион',
+				value: locationInfo.region,
+				BXName: 'UF_CRM_1517920515',
+			},
+			{
+				name: 'IP',
+				value: locationInfo.ip,
+			},
+		],
+	};
+	if (fields) {
+		data.fields = [...data.fields, ...fields];
+	}
+	return await axios.post(url, data, {
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		}
+	});
 }
 
 export interface field {
-    name: string;
-    value: string;
-    BXName?: string;
+	name: string;
+	value: string;
+	BXName?: string;
 }
 interface locationData {
-    ip: string;
-    region: string;
-    country: string;
+	ip: string;
+	region: string;
+	country: string;
 }
 
 // async function getLocationData() {
@@ -88,26 +93,26 @@ interface locationData {
 // }
 
 async function getLocationData() {
-    let locationData: locationData | {} = {
-        ip: '',
-        region: '',
-        country: '',
-        city: '',
-    };
-    await axios
-        .get(
-            'https://api.ipgeolocation.io/ipgeo?apiKey=2e4dabeb35b6489d9348d88276585aee'
-        )
-        .then((response: any) => {
-            locationData = {
-                ip: response.data.ip,
-                city: response.data.city,
-                region: response.data.state_prov,
-                country: response.data.country_name,
-            };
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    return locationData;
+	let locationData: locationData | {} = {
+		ip: '',
+		region: '',
+		country: '',
+		city: '',
+	};
+	await axios
+		.get(
+			'https://api.ipgeolocation.io/ipgeo?apiKey=2e4dabeb35b6489d9348d88276585aee',
+		)
+		.then((response: any) => {
+			locationData = {
+				ip: response.data.ip,
+				city: response.data.city,
+				region: response.data.state_prov,
+				country: response.data.country_name,
+			};
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	return locationData;
 }
