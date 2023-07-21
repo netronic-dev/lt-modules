db855898208a960e09611b9f9bc87d45f9253b10
Artem <yatsenko@netronic.com.ua>
2023-07-21 09:36:55 +0300

    Update summer-2023

--- SummerSale/Count/index.jsx
+++ SummerSale/Count/index.jsx
@@ -3,51 +3,52 @@ import style from './style.module.scss';
 import Image from 'next/image';
 
 const Count = (props) => {
-	return (
-		<section className={style.main}>
-			<div className={style.content}>
-				<h2 className={style.title}>{props.title}</h2>
-				<div className={style.counter_wrapper}>
-					<div className={style.counter_item}>3</div>
-					<div className={style.counter_item}>6</div>
-				</div>
-				<span className={style.text}>{props.text}</span>
-				<AnchorLink href='#sale' offset='200'>
-					<button className={style.button}>{props.buttonText}</button>
-				</AnchorLink>
-				<div className={style.under_button_text}>
-					{props.underButtonText}
-				</div>
-			</div>
-			<div className={style.bacground_text_wrap}>
-				<p className={style.bacground_text}>{props.bacgroundText}</p>
-			</div>
-			<div className={style.right_ribbons}>
-				<Image
-					src={props.right_ribons}
-					alt='ribbons'
-					layout='fill'
-					objectFit='cover'
-				/>
-			</div>
-			<div className={style.left_ribbons}>
-				<Image
-					src={props.left_ribons}
-					alt='ribbons'
-					layout='fill'
-					objectFit='cover'
-				/>
-			</div>
-			<div className={style.left_ribbon_mob}>
-				<Image
-					src={props.left_ribon_mob}
-					alt='ribbons'
-					layout='fill'
-					objectFit='cover'
-				/>
-			</div>
-		</section>
-	);
+    return (
+        <section className={style.main}>
+            <div className={style.content}>
+                <h2 className={style.title}>{props.title}</h2>
+                <div className={style.counter_wrapper}>
+                    <div className={style.counter_item}>0</div>
+                </div>
+                <span className={style.text}>{props.text}</span>
+                <AnchorLink href='#sale' offset='200'>
+                    <button className={style.button} disabled={true}>
+                        {props.buttonText}
+                    </button>
+                </AnchorLink>
+                <div className={style.under_button_text}>
+                    {props.underButtonText}
+                </div>
+            </div>
+            <div className={style.bacground_text_wrap}>
+                <p className={style.bacground_text}>{props.bacgroundText}</p>
+            </div>
+            <div className={style.right_ribbons}>
+                <Image
+                    src={props.right_ribons}
+                    alt='ribbons'
+                    layout='fill'
+                    objectFit='cover'
+                />
+            </div>
+            <div className={style.left_ribbons}>
+                <Image
+                    src={props.left_ribons}
+                    alt='ribbons'
+                    layout='fill'
+                    objectFit='cover'
+                />
+            </div>
+            <div className={style.left_ribbon_mob}>
+                <Image
+                    src={props.left_ribon_mob}
+                    alt='ribbons'
+                    layout='fill'
+                    objectFit='cover'
+                />
+            </div>
+        </section>
+    );
 };
 
 export default Count;
--- SummerSale/Main/index.jsx
+++ SummerSale/Main/index.jsx
@@ -5,66 +5,79 @@ import WordSlider from '../WordSlider';
 import Link from 'next/link';
 
 const Main = (props) => {
-	return (
-		<section className={style.main}>
-			<div className={style.main_inner}>
-				<div className={style.content}>
-					<h1 className={style.title}>{props.title}</h1>
-					<p className={style.text}>{props.text}</p>
-					{props.textUS ? (
-						<div className={style.us_block}>
-							<p className={style.us_text}>{props.textUS}</p>
-							<div className={style.flag}>
-								<Image
-									src={props.flag}
-									alt='usa flag'
-									layout='fill'
-									objectFit='cover'
-								/>
-							</div>
-						</div>
-					) : null}
-					<AnchorLink href='#sale' offset='200'>
-						<button className={style.button}>
-							{props.buttonText}
-						</button>
-					</AnchorLink>
-				</div>
-				<div className={style.image_outer}>
-					<Image
-						src={props.image}
-						layout='fill'
-						objectFit='cover'
-						objectPosition={props.objectPosition}
-						priority={true}
-					/>
-				</div>
-				<div className={style.image_outer_lap}>
-					<Image
-						src={props.image_lap}
-						layout='fill'
-						objectFit='cover'
-						objectPosition={props.objectPosition}
-						priority={true}
-					/>
-				</div>
-				<div className={style.image_outer_mob}>
-					<Image
-						src={props.image_mob}
-						layout='fill'
-						objectFit='cover'
-						objectPosition='0 0'
-						priority={true}
-					/>
-				</div>
-			</div>
-			<WordSlider
-				blueText
-				className={style.words_slider}
-				text={props.sliderWordsText}
-			/>
-		</section>
-	);
+    return (
+        <section className={style.main}>
+            <div className={style.main_inner}>
+                <div className={style.content}>
+                    <div className={style.title_block}>
+                        <h1 className={style.title}>{props.title}</h1>
+                        <div className={style.image_sold_out}>
+                            <Image
+                                src={props.image_sold_out}
+                                alt='Sold-out'
+                                layout='fill'
+                                objectFit='contain'
+                                // width={230}
+                                // height={138}
+                                priority={true}
+                            />
+                        </div>
+                    </div>
+                    <p className={style.text}>{props.text}</p>
+                    {props.textUS ? (
+                        <div className={style.us_block}>
+                            <p className={style.us_text}>{props.textUS}</p>
+                            <div className={style.flag}>
+                                <Image
+                                    src={props.flag}
+                                    alt='usa flag'
+                                    layout='fill'
+                                    objectFit='cover'
+                                />
+                            </div>
+                        </div>
+                    ) : null}
+                    <AnchorLink href='#sale' offset='200'>
+                        <button className={style.button} disabled={true}>
+                            {props.buttonText}
+                        </button>
+                    </AnchorLink>
+                </div>
+                <div className={style.image_outer}>
+                    <Image
+                        src={props.image}
+                        layout='fill'
+                        objectFit='cover'
+                        objectPosition={props.objectPosition}
+                        priority={true}
+                    />
+                </div>
+                <div className={style.image_outer_lap}>
+                    <Image
+                        src={props.image_lap}
+                        layout='fill'
+                        objectFit='cover'
+                        objectPosition={props.objectPosition}
+                        priority={true}
+                    />
+                </div>
+                <div className={style.image_outer_mob}>
+                    <Image
+                        src={props.image_mob}
+                        layout='fill'
+                        objectFit='cover'
+                        objectPosition='0 0'
+                        priority={true}
+                    />
+                </div>
+            </div>
+            <WordSlider
+                blueText
+                className={style.words_slider}
+                text={props.sliderWordsText}
+            />
+        </section>
+    );
 };
 
 export default Main;
