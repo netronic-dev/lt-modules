7def4b7ff2fd5e4763ace4730242f88569e9320f
artemyatsenko91 <aliens199@ukr.net>
2024-04-18 15:55:00 +0300

    fix: change iplocation service and add ip data to cookie

--- functions/postData.ts
+++ functions/postData.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { getLocalStorage } from "../CookieBanner/storageHelper";
 
-export const fbpCookie = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("_fbp="))
-    ?.split("=")[1];
+const getCookieByKey = (key: string) => {
+    if (typeof window !== "undefined") {
+        return document.cookie
+            .split("; ")
+            .find((row) => row.startsWith(`${key}=`))
+            ?.split("=")[1];
+    }
+};
+
+export const fbpCookie = getCookieByKey("_fbp");
 
 export async function postData(
     values: any,
@@ -102,6 +108,34 @@ interface locationData {
     state: string;
 }
 
+// export async function getLocationData() {
+//     let locationData = {
+//         ip: "",
+//         city: "",
+//         region: "",
+//         country: "",
+//         zipcode: "",
+//         state: "",
+//     };
+//     await axios
+//         .get(
+//             "https://api.ipgeolocation.io/ipgeo?apiKey=2e4dabeb35b6489d9348d88276585aee"
+//         )
+//         .then((response) => {
+//             locationData = {
+//                 ip: response.data.ip,
+//                 city: response.data.city,
+//                 region: response.data.country_code2,
+//                 country: response.data.country_name,
+//                 zipcode: response.data.zipcode,
+//                 state: response.data.state_prov,
+//             };
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+//     return locationData;
+// }
 export async function getLocationData() {
     let locationData = {
         ip: "",
@@ -111,22 +145,42 @@ export async function getLocationData() {
         zipcode: "",
         state: "",
     };
-    await axios
-        .get(
-            "https://api.ipgeolocation.io/ipgeo?apiKey=2e4dabeb35b6489d9348d88276585aee"
-        )
-        .then((response) => {
-            locationData = {
-                ip: response.data.ip,
-                city: response.data.city,
-                region: response.data.country_code2,
-                country: response.data.country_name,
-                zipcode: response.data.zipcode,
-                state: response.data.state_prov,
-            };
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    let cookieIp = getCookieByKey("ip");
+
+    const clientIP = await axios.get("https://api.ipify.org/?format=json");
+    if (clientIP.data.ip === cookieIp) {
+        locationData = {
+            ip: getCookieByKey("ip") || "",
+            city: getCookieByKey("city") || "",
+            region: getCookieByKey("region") || "",
+            country: getCookieByKey("country") || "",
+            zipcode: getCookieByKey("zipcode") || "",
+            state: getCookieByKey("state") || "",
+        };
+    } else {
+        await axios
+            .get("https://ipinfo.io/json?token=ee40c07fb51963")
+            .then((response) => {
+                locationData = {
+                    ip: response.data.ip,
+                    city: response.data.city,
+                    region: response.data.country,
+                    country: response.data.country,
+                    zipcode: response.data.postal,
+                    state: response.data.region,
+                };
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        let date = new Date(
+            Date.now() + 30 * 24 * 60 * 60 * 1000
+        ).toUTCString();
+        for (const [key, value] of Object.entries(locationData)) {
+            document.cookie = `${key}=${value}; expires="${date}"`;
+        }
+    }
+
     return locationData;
 }
