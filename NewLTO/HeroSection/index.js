5801c8b0f92e8cf77826208588c9c6a454ce5765
Albina Anistratenko <albuska2013@gmail.com>
2024-06-17 08:50:08 +0300

    change bg img

--- NewLTO/HeroSection/index.js
+++ NewLTO/HeroSection/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import style from "./style.module.scss";
 import heroLogo from "/public/images/newLTO/lto-logo.webp";
 import heroBgImage from "/public/images/newLTO/heroImg.webp";
+import heroNightBg from "/public/images/newLTO/galaxy-night.webp";
 import ChangeLanguage from "../ChangeLanguage/index";
 import { useIntersectionObserver } from "../../../hooks/useIntersectionObserver";
 
@@ -18,7 +19,7 @@ const HeroSection = ({ title, titleSpan, subtitle, text, textSpan }) => {
   );
 
   return (
-    <section className={style.hero}>
+    <section className={style.hero} style={{ backgroundImage: `url(${heroNightBg.src})` }}>
       <div className={style.hero__insideBox}>
         <div className={style.hero__changeLanguageBox}>
           <ChangeLanguage />
