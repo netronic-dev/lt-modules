0c55c5ae24372e9cd92a264d065aab39c8662c80
Albina Anistratenko <albuska2013@gmail.com>
2025-01-09 19:28:46 +0200

    delete console.logs in obfuscateEmail function

--- functions/obfuscateEmail.js
+++ functions/obfuscateEmail.js
@@ -1,12 +1,4 @@
 export const obfuscateEmail = (email) => {
-  console.log(email, "email");
-  console.log(
-    email
-      .split("")
-      .map((char) => `&#${char.charCodeAt(0)};`)
-      .join(""),
-    "email change"
-  );
   return email
     .split("")
     .map((char) => `&#${char.charCodeAt(0)};`)
