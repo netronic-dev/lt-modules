6a1892988cf31193f10375129b5efda2b839e66d
albuska <albuska2013@gmail.com>
2025-05-30 14:05:18 +0300

    change phone parameter in sendEventToConversionApi function

--- functions/sendFbPageView.js
+++ functions/sendFbPageView.js
@@ -1,34 +1,81 @@
+// import axios from "axios";
+// import { getLocationData, fbpCookie } from "./postData";
+
+// export const sendEventToConversionApi = async (
+//   siteName,
+//   eventName,
+//   userData = {}
+// ) => {
+//   const userLocationData = await getLocationData();
+//   const userAgent = navigator.userAgent;
+
+//   const eventPayload = {
+//     eventName: eventName,
+//     eventUrl: siteName,
+//     userData: {
+//       email: userData.email,
+//       phone: userData.phone,
+//       city: userLocationData.city,
+//       region: userLocationData.region,
+//       country: userLocationData.country,
+//       zip: userLocationData.zipcode,
+//       ip: userLocationData.ip,
+//       userAgent: userAgent,
+//       fbc: fbpCookie,
+//       fbp: undefined,
+//     },
+//   };
+
+//   try {
+//     const response = await axios.post(
+//       "https://back.netronic.net/facebook/event",
+//       eventPayload,
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+
+//     console.log("✅ Facebook event sent successfully:", response.data);
+//   } catch (error) {
+//     await axios.post("https://back.netronic.net/telegram/send-error-message", {
+//       message: `❌ Error sending Facebook event ${window.location.hostname}: ${error}`,
+//     });
+//   }
+// };
+
 import axios from "axios";
-import { fbpCookie, getLocationData } from "./postData";
+import { getLocationData, fbpCookie } from "./postData";
 
 export const sendEventToConversionApi = async (
   siteName,
   eventName,
   userData = {}
 ) => {
-  const userLocationData = await getLocationData();
+  const userLocationData = (await getLocationData()) || {};
   const userAgent = navigator.userAgent;
 
   const eventPayload = {
-    eventName: eventName,
+    eventName,
     eventUrl: siteName,
     userData: {
-      email: userData.email,
-      phone: userData.phone,
-      city: userLocationData.city,
-      region: userLocationData.region,
-      country: userLocationData.country,
-      zip: userLocationData.zipcode,
-      ip: userLocationData.ip,
-      userAgent: userAgent,
-      fbc: fbpCookie,
-      fbp: undefined,
+      ...userData,
+      city: userLocationData.city || "",
+      region: userLocationData.region || "",
+      country: userLocationData.country || "",
+      zip: userLocationData.zipcode || "",
+      ip: userLocationData.ip || "",
+      userAgent,
+      fbc: fbpCookie || "",
+      fbp: fbpCookie || "",
     },
   };
 
   try {
     const response = await axios.post(
-      "https://back.netronic.net/facebook/event",
+      "https://back.netronic.net/facebook/event",                       
+      // "http://localhost:5002/facebook/event",                       
       eventPayload,
       {
         headers: {
