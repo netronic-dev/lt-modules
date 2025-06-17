e4b3ec32676c1719f749aeb608476d4b4e5f46c4
albuska <albuska2013@gmail.com>
2025-06-17 17:31:54 +0300

    change sendEventToConversionApi

--- functions/postData.ts
+++ functions/postData.ts
@@ -35,8 +35,6 @@ export async function postData(
     referrer = localStorage.getItem("referrer") || "";
   }
 
-  console.log(referrer, "referrer");
-
   const query = {
     utm_campaign: routerQuery.utm_campaign || "",
     utm_medium: routerQuery.utm_medium || "",
@@ -68,7 +66,7 @@ export async function postData(
       city: locationInfo.city,
     },
     source: source || "",
-    // referrer,
+    referrer,
     language: "Англійська",
     chatId: values.chatId || "",
   };
