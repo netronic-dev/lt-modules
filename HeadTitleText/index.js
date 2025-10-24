11148508c768996ac5944b413f7ead15473b3cc2
Netronic Developer <115734128+netronic-dev@users.noreply.github.com>
2025-10-24 16:44:47 +0300

    change /equipment/sets/outdoor-sets page

--- HeadTitleText/index.js
+++ HeadTitleText/index.js
@@ -1,17 +1,18 @@
-import style from './style.module.scss'
+import style from "./style.module.scss";
 
 const styles = {
-  "white": style.block_white,
-  "light": style.block_light,
-  "dark": style.block_dark,
-  "productCard": style.block_product_card
-}
+  white: style.block_white,
+  white_new: style.block_white_new,
+  light: style.block_light,
+  dark: style.block_dark,
+  productCard: style.block_product_card,
+};
 
 export default function HeadTitleText(props) {
   return (
     <section className={styles[props.style]}>
       <h1 className={style.title}>{props.title}</h1>
-      <p className={style.text}>{props.text}</p>
+      {props.text && <p className={style.text}>{props.text}</p>}
     </section>
-  )
+  );
 }
