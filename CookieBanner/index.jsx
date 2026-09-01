556f05228303276f5b82532fd957532998da2c14
netronic-dev <dev.netronic@gmail.com>
2026-09-01 11:17:36 +0300

    add google tag gateway

--- CookieBanner/index.jsx
+++ CookieBanner/index.jsx
@@ -2,57 +2,68 @@ import "client-only";
 
 import style from "./style.module.scss";
 import { getLocalStorage, setLocalStorage } from "./storageHelper";
-import { useState, useEffect, useLayoutEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 export default function CookieBanner(props) {
-    const [cookieConsent, setCookieConsent] = useState(null);
+  const [cookieConsent, setCookieConsent] = useState(null);
 
-    useLayoutEffect(() => {
-        const storedCookieConsent = getLocalStorage("cookie_consent", null);
+  useLayoutEffect(() => {
+    const storedCookieConsent = getLocalStorage("cookie_consent", null);
+    setCookieConsent(storedCookieConsent);
+  }, []);
 
-        setCookieConsent(storedCookieConsent);
-    }, [setCookieConsent]);
+  const updateConsent = (isGranted) => {
+    const newValue = isGranted ? "granted" : "denied";
 
-useEffect(() => {
-  if (typeof window.gtag === "function") {
-    const newValue = cookieConsent ? "granted" : "denied";
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
 
-    window.gtag("consent", "update", {
-      analytics_storage: newValue,
-      ad_storage: newValue,
-    });
+      gtag("consent", "update", {
+        analytics_storage: newValue,
+        ad_storage: newValue,
+        ad_user_data: newValue,
+        ad_personalization: newValue,
+      });
 
-    setLocalStorage("cookie_consent", cookieConsent);
-  } else {
-    console.warn("Google Analytics is not loaded yet.");
+      window.dataLayer.push({
+        event: "cookie_consent_update",
+        cookie_consent_status: newValue,
+      });
+    }
+
+    setLocalStorage("cookie_consent", isGranted);
+    setCookieConsent(isGranted);
+  };
+
+  if (cookieConsent !== null) {
+    return null;
   }
-    }, [cookieConsent]);
 
-    return cookieConsent === null ? (
-        <div className={style.banner}>
-            <div className={style.container}>
-                <div className={style.content}>
-                    <h3 className={style.banner_title}>{props.title}</h3>
-                    <p className={style.banner_text}>{props.text}</p>
-                </div>
-                <div className={style.buttons_block}>
-                    <button
-                        className={style.allow_button}
-                        onClick={() => {
-                            setCookieConsent(true);
-                            window.location.reload();
-                        }}
-                    >
-                        {props.allow_btn_text}
-                    </button>
-                    <button
-                        className={style.decline_button}
-                        onClick={() => setCookieConsent(false)}
-                    >
-                        {props.decline_btn_text}
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className={style.banner}>
+      <div className={style.container}>
+        <div className={style.content}>
+          <h3 className={style.banner_title}>{props.title}</h3>
+          <p className={style.banner_text}>{props.text}</p>
         </div>
-    ) : null;
+        <div className={style.buttons_block}>
+          <button
+            className={style.allow_button}
+            onClick={() => updateConsent(true)}
+          >
+            {props.allow_btn_text}
+          </button>
+          <button
+            className={style.decline_button}
+            onClick={() => updateConsent(false)}
+          >
+            {props.decline_btn_text}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
